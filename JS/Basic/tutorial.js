27e299f686b55658f75e5ce2965c33b055a59c4d
jjy0328 <wjdwndud3907@naver.com>
2023-06-09 23:23:11 +0900

    TIL javascript

---
+++ JS/Basic/tutorial.js
@@ -0,0 +1,8 @@
+// 자바스크립트의 공식사이트 : https://developer.mozilla.org/ko/docs/Web/JavaScript
+// print문
+
+// 순수 js를 사용하는 경우
+// 선언되지 않은 변수에 값을 할당하는 경우가 생길 수 있어 이를 방지함.
+'use strict';
+
+console.log('Hello World!');
